80dbe5830446a3b356f98943d831094f0a63723e
Cat Nuguid <catnuguid@gmail.com>
2020-11-10 17:05:55 +0900

    reconfig webpack file-loader for background-image

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,6 +8,8 @@ function Homepage() {
       <div className="welcome-text-container">
         <h1 className="welcome-text">Welcome to BoopCamp!</h1>
       </div>
+
+      {/* when button is clicked, it will redirect to page showing all campsites */}
       <div>
         <Link to="/campsites">
           <button className="view-button" alt="view campsites">
