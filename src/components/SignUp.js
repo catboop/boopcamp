ccfb9a193b8c345f5d79a39e1433b0329079f2e0
Cat Nuguid <catnuguid@gmail.com>
2020-11-11 20:21:57 +0900

    created signup component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 
+// TODO
+// pass props to react router to reuse signin component for signin and signup windows
+// add logic to post new user to userApi
+
 function SignUp() {
+
   return (
-    <div>
-        Sign up
+    <div className="welcome login-container">
+        <div className="login-window">
+          <h2 className="login-header">Please sign up:</h2>
+          <div className="login-input-container">
+            <label for="username">Username*</label>
+            <input className="login-input" type="text" name="username" required/>
+          </div>
+
+          <div className="login-input-container">
+            <label for="password">Password*</label>
+            <input className="login-input" type="text" name="password" required/>
+          </div>
+
+          <div className="login-input-container">
+            <button className="login-button">Submit</button>
+          </div>
+        </div>
     </div>
   );
 }
