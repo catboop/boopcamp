fe2bd9009b6ef9d01060601cde4a3334a76f6861
Cat Nuguid <catnuguid@gmail.com>
2020-11-17 19:33:43 +0900

    campsitesAPI controller to get all campsites

--- server/controllers/campsitesAPI.js
+++ server/controllers/campsitesAPI.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../knex");
+const db = require("../knex.js");
 
+// get all campsites
 router.get("/", async (_, res) => {
-    res.send("campApi working");
+  try {
+    const campsites = await db.select("*").table("campsites");
+    console.log("campsites: ", campsites);
+    res.send(campsites);
+  } catch (err) {
+    //If error occur, send 500 status code
+    console.log(err)
+    res.sendStatus(500);
+  }
 });
+
+// add new campsite
+
+// edit existing campsite
+
+// delete existing campsite
 
 module.exports = router;
