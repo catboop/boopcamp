6e1dc571f05aa468d9b9161b28c2912ea4d4ab22
Cat Nuguid <catnuguid@gmail.com>
2020-11-20 21:17:00 +0900

    wrote controllers for login/register

--- server/controllers/userAPI.js
+++ server/controllers/userAPI.js
@@ -1,10 +1,62 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../knex");
+const db = require("../knex.js");
 const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-router.get("/", async (_, res) => {
-    res.send("userApi working");
+// TODO : create token and send back during login request
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    // check if username already exists
+    const user = await db("users").select("*").where({username});
+    if (user.length > 0) {
+      res.json("Username already exists.");
+    };
+
+    // create hash of pw to store in db
+    const password_digest = await bcrypt.hash(password, saltRounds);
+
+    // insert into db
+    await db("users").insert({ username, password_digest, token: "token" })
+
+    res.status(200).json("token");
+    
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Unable to register. Try again later.")
+  }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body
+    
+    // get user from database
+    const user = await db("users").select("*").where({username});
+    
+    // check if user exists
+    if (user.length > 0) {
+
+      // check if password is correct
+      const validPassword = await bcrypt.compare(password, user[0].password_digest);      
+      if (validPassword) {
+        res.status(200).json("token")
+      } else {
+        res.json("Wrong password!")
+      }
+
+    // if user doesn't exist in db
+    } else {
+      res.status(404).json("User not found.")
+    }
+    
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Unable to log in. Try again later.")
+  }
+})
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const app = express();
+const app = express(); // app definition
 const path = require('path'); 
 const port = process.env.PORT || 4000;
 const cors = require("cors");
 
+// controllers
 const UserAPI = require("./controllers/userAPI");
 const CampsitesAPI = require("./controllers/campsitesAPI");
 
+// middleware setup
 app.use(express.json());
-
 app.use(cors());
 
 app.use("/api/users", UserAPI);
