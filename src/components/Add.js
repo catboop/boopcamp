689c92a359f2dccc076254f7c721f632a814f009
Cat Nuguid <catnuguid@gmail.com>
2020-11-27 23:50:51 +0900

    adding event to input fields to add new site

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { hot } from "react-hot-loader";
 
 function Add( {setDisplayAdd} ) {
+  const [ inputs, setInputs ] = useState({});
+
   return (
     <div className="add-container">
 
@@ -12,6 +14,7 @@ function Add( {setDisplayAdd} ) {
           className="new-input"
           name="name"
           placeholder="Konashidaira"
+          onInput={e => setInputs({...inputs, name: e.target.value})}
           required
         />
       </div>
@@ -23,6 +26,7 @@ function Add( {setDisplayAdd} ) {
           className="new-input"
           name="address"
           placeholder="Kamikochi, Nagano, Japan 390-1516"
+          onInput={e => setInputs({...inputs, address: e.target.value})}
         />
       </div>
 
@@ -33,6 +37,7 @@ function Add( {setDisplayAdd} ) {
           className="new-input"
           name="image"
           placeholder="www.image-url.com"
+          onInput={e => setInputs({...inputs, url_to_image: e.target.value})}
         />
       </div>
 
@@ -43,6 +48,7 @@ function Add( {setDisplayAdd} ) {
           className="new-input"
           name="website"
           placeholder="www.nihonalpskankou.com"
+          onInput={e => setInputs({...inputs, website: e.target.value})}
         />
       </div>
 
@@ -53,6 +59,7 @@ function Add( {setDisplayAdd} ) {
           className="new-input"
           name="description"
           placeholder="Great for mountain-lovers!"
+          onInput={e => setInputs({...inputs, description: e.target.value})}
         />
       </div>
 
